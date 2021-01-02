6de404778032e5f6d567545542f8e692dfc9b30e
Hugo Protsch <hugo.protsch@gmail.com>
2021-01-02 13:00:44 +0100

    Code: Add first implementation of splaytree

--- src/splaytree.erl
+++ src/splaytree.erl
@@ -1,4 +1,76 @@
 -module(splaytree).
 -author("Hugo Protsch").
 
--export([]).
+-export([findBT2/2, findBT/2, equalBT/2, isEmptyBT/1, initBT/0, insertBT2/2, insertBT/2, printBT/2]).
+
+initBT() -> avltree:initBT().
+
+isEmptyBT(BTree) -> avltree:isEmptyBT(BTree).
+
+equalBT(BTree, BTree) -> avltree:equalBT(BTree, BTree).
+
+printBT(BTree, Filename) -> avltree:printBT(BTree, Filename).
+
+findBT({SearchEl, H, L, R}, SearchEl) -> {H, {SearchEl, H, L, R}};
+findBT(BTree, SearchEl) ->
+  {NewBTree, D, FoundHeight} = findBT2(BTree, SearchEl),
+  if
+    (D == notfound) -> {BTree, 0};
+    true -> {FoundHeight, zigIfNeeded({NewBTree, D})}
+  end.
+
+insertBT(BTree, InsertElement) ->
+  zigIfNeeded(insertBT2(BTree, InsertElement)).
+
+findBT2({El, H, L, R}, SearchEl) when SearchEl < El ->
+  {NewL, D, FoundHeight} = findBT2(L, SearchEl),
+  NewRoot = {El, H, NewL, R},
+  if
+    (D == here) -> {NewRoot, left, FoundHeight};
+    (D == notfound) -> {NewRoot, notfound, FoundHeight};
+    true -> {splay(NewRoot, left, D), here, FoundHeight}
+  end;
+findBT2({SearchEl, H, L, R}, SearchEl) -> {{SearchEl, H, L, R}, here, H};
+findBT2({El, H, L, R}, SearchEl) ->
+  {NewR, D, FoundHeight} = findBT2(R, SearchEl),
+  NewRoot = {El, H, L, NewR},
+  if
+    (D == here) -> {NewRoot, right, FoundHeight};
+    (D == notfound) -> {NewRoot, notfound, FoundHeight};
+    true -> {splay(NewRoot, right, D), here, FoundHeight}
+  end;
+findBT2({}, _) -> {{}, notfound, 0}.
+
+insertBT2({El, H, L, R}, InsertEl) when InsertEl < El ->
+  {NewL, D} = insertBT2(L, InsertEl),
+  NewRoot = {El, -1, NewL, R }, % height is recalculated when rotating
+  if
+    (D == here) -> {NewRoot, left};
+    true -> {splay(NewRoot, left, D), here}
+  end;
+insertBT2({El, H, L, R}, InsertEl) when InsertEl > El ->
+  {NewR, D} = insertBT2(R, InsertEl),
+  NewRoot = {El, -1, L, NewR}, % height is recalculated when rotating
+  if
+    (D == here) -> {NewRoot, right};
+    true -> {splay(NewRoot, right, D), here}
+  end;
+insertBT2({}, InsertEl) -> {{InsertEl, 1, {}, {}}, here};
+insertBT2({InsertEl, H, L, R}, InsertEl) -> {{InsertEl, H, L, R}, here}.
+
+splay(Root, left, left) ->
+  NewRoot = avltree:rotateR(Root),
+  avltree:rotateR(NewRoot);
+splay(Root, right, right) ->
+  NewRoot = avltree:rotateL(Root),
+  avltree:rotateL(NewRoot);
+splay({El, H, L, R}, right, left) -> avltree:rotateL({El, H, L, avltree:rotateR(R)});
+splay({El, H, L, R}, left, right) -> avltree:rotateR({El, H, avltree:rotateL(L), R}).
+
+zigIfNeeded({NewBTree, D}) ->
+  if
+    (D == here) -> NewBTree;
+    (D == left) -> avltree:rotateR(NewBTree);
+    (D == right) -> avltree:rotateL(NewBTree);
+    true -> err
+  end.
--- src/splaytreeTest.erl
+++ src/splaytreeTest.erl
@@ -2,6 +2,119 @@
 -author("Hugo Protsch").
 
 -include_lib("eunit/include/eunit.hrl").
+-import(splaytree, [initBT/0, insertBT/2]).
 
-simple_test() ->
-  ?assert(true).
+print_test() ->
+  Tree = treeInsertFromList(initBT(),util:randomliste(100)),
+  splaytree:printBT(Tree, 'splay100Random.gv').
+
+notfound_test() ->
+  ?assertEqual({{}, notfound, 0}, splaytree:findBT2({}, 1)),
+  ?assertEqual({{}, 0}, splaytree:findBT({}, 1)),
+  ?assertEqual({tree4(), 0}, splaytree:findBT(tree4(), 1)),
+  ?assertEqual({tree4(), notfound, 0}, splaytree:findBT2(tree4(), 1)).
+
+insertAlreadyThere_test() ->
+  ?assertEqual(tree2(), insertBT(tree2(), 1000)),
+  ?assertEqual(tree2_find250(), insertBT(tree2(), 250)).
+
+insert_test() ->
+  Insert100 = {100, 1, {}, {}},
+  Insert200 = {200, 2, {100, 1, {}, {}}, {}},
+  Insert150 = {150, 2, {100, 1, {}, {}}, {200, 1, {}, {}}},
+  Insert125 = {125, 3, {100, 1, {}, {}}, {150, 2, {}, {200, 1, {}, {}}}},
+  ?assertEqual(Insert100, splaytree:insertBT(initBT()
+    , 100)),
+  ?assertEqual(Insert200, splaytree:insertBT(splaytree:insertBT(initBT(),
+    100), 200)),
+  ?assertEqual(Insert150, splaytree:insertBT(splaytree:insertBT(splaytree:insertBT(initBT(),
+    100), 200), 150)),
+  ?assertEqual(Insert125,
+    splaytree:insertBT(splaytree:insertBT(splaytree:insertBT(avltree:insertBT(initBT(),
+      100), 200), 150), 125))
+.
+
+
+zigzig_test() ->
+  initBT(),
+  {Tree2Find250, here, 1} = splaytree:findBT2(tree2(), 250),
+  ?assertEqual(2, util:countread(rightrotate)),
+  ?assertEqual(tree2_find250(), Tree2Find250),
+  initBT(),
+  {1, Tree2Find250} = splaytree:findBT(tree2(), 250),
+  ?assertEqual(2, util:countread(rightrotate)),
+  ?assertEqual(tree2_find250(), Tree2Find250),
+
+  initBT(),
+  {Tree2, here, 1} = splaytree:findBT2(tree2_find250(), 1000),
+  ?assertEqual(2, util:countread(leftrotate)),
+  ?assertEqual(tree2(), Tree2),
+  initBT(),
+  {1, Tree2} = splaytree:findBT(tree2_find250(), 1000),
+  ?assertEqual(2, util:countread(leftrotate)),
+  ?assertEqual(tree2(), Tree2).
+
+zigzagLR_test() ->
+  initBT(),
+  {Tree3Find75, here, 1} = splaytree:findBT2(tree3(), 75),
+  ?assertEqual(1, util:countread(leftrotate)),
+  ?assertEqual(1, util:countread(rightrotate)),
+  ?assertEqual(tree3_find75(), Tree3Find75),
+  initBT(),
+  {1, Tree3Find75} = splaytree:findBT(tree3(), 75),
+  ?assertEqual(1, util:countread(leftrotate)),
+  ?assertEqual(1, util:countread(rightrotate)),
+  ?assertEqual(tree3_find75(), Tree3Find75).
+
+
+zigzagRL_test() ->
+  initBT(),
+  {Tree4Find500, here, 1} = splaytree:findBT2(tree4(), 500),
+  ?assertEqual(1, util:countread(leftrotate)),
+  ?assertEqual(1, util:countread(rightrotate)),
+  ?assertEqual(tree4_find500(), Tree4Find500),
+  initBT(),
+  {1, Tree4Find500} = splaytree:findBT(tree4(), 500),
+  ?assertEqual(1, util:countread(leftrotate)),
+  ?assertEqual(1, util:countread(rightrotate)),
+  ?assertEqual(tree4_find500(), Tree4Find500).
+
+
+tree2() ->
+  {1000, 3,
+    {500, 2,
+      {250, 1, {}, {}},
+      {}},
+    {}
+  }.
+
+tree2_find250() ->
+  {
+    250, 3, {}, {
+    500, 2, {}, {
+      1000, 1, {}, {}}
+  }}
+.
+
+tree3() ->
+  {100, 3,
+    {50, 2,
+      {},
+      {75, 1, {}, {}}
+    },
+    {}
+  }.
+
+tree3_find75() -> {75, 2, {50, 1, {}, {}}, {100, 1, {}, {}}}.
+
+tree4() ->
+  {250, 3,
+    {},
+    {1000, 2, {500, 1, {}, {}}, {}}
+  }.
+
+tree4_find500() -> {500, 2, {250, 1, {}, {}}, {1000, 1, {}, {}}}.
+
+treeInsertFromList(Tree, [H | T]) ->
+  treeInsertFromList(splaytree:insertBT(Tree, H), T);
+treeInsertFromList(Tree, []) -> Tree.
