d891ad979c4fabce54d941907df5609017ab0978
Hugo Protsch <hugo.protsch@gmail.com>
2021-01-28 22:15:17 +0100

    Tex: Zeitmessung avl

--- src/zeitmessung.erl
+++ src/zeitmessung.erl
@@ -2,40 +2,49 @@
 -author("hugop").
 
 %% API
--export([start/0]).
+-export([start/0, splay/0, avl/0]).
 
 start() ->
   io:format("- - - - - - - - - - STARTING - - - - - - - - - - ~n"),
   avl(),
+  splay(),
   io:format("- - - - - - - - - - FINISHED - - - - - - - - - - ~n")
-  .
-
-avl() -> io:format("- - - - - - - - - - - AVL - - - - - - - - - - - ~n"),
-  avl1(),
-  avl2()
 .
 
-avl1() ->
-  messung(avl, 500, 1000, 50, 20, rand),
-  messung(avl, 500, 1000, 50, 20, auf),
-  messung(avl, 500, 1000, 50, 20, ab).
+mittlen_ueber() -> 20.
 
-avl2() ->
-  messung(avl, 10000, 10000, 100, 10, rand),
-  messung(avl, 10000, 10000, 100, 10, auf),
-  messung(avl, 10000, 10000, 100, 10, ab).
+avl() -> io:format("- - - - - - - - - - - AVL - - - - - - - - - - - ~n"),
+  avl(500, 1000, 50)   % 0 - 50_000
+.
+splay() -> io:format("- - - - - - - - - - SPLAY - - - - - - - - - - ~n"),
+  messung(splay, 500, 1000, 20, rand),
+  messung(splay, 500, 1000, 5, auf),
+  messung(splay, 500, 1000, 5, ab)
+.
 
-messung(Typ, Start, Schrittgr, AnzSchritte, MittelnUeber, RandAufAb) ->
-  io:format("-----------------------------------------------~n"),
+avl(Start, Schritg, Anz) ->
+  messung(avl, Start, Schritg, Anz, rand),
+  messung(avl, Start, Schritg, Anz, auf),
+  messung(avl, Start, Schritg, Anz, ab).
+
+splay(Start, Schritg, Anz) ->
+  messung(splay, Start, Schritg, Anz, rand),
+  messung(splay, Start, Schritg, Anz, auf),
+  messung(splay, Start, Schritg, Anz, ab).
+
+messung(Typ, Start, Schrittgr, AnzSchritte, RandAufAb) ->
+  io:format("---------------------------------------------------------------------------------------~n"),
   io:format(atom_to_list(Typ)),
   io:format(" mit "),
   io:format(atom_to_list(RandAufAb)),
   io:format(" zahlen~n~p +~p (~p mal), mittel ueber ~p~n",
-    [Start,Schrittgr,AnzSchritte, MittelnUeber]),
-  io:format("-----------------------------------------------~n"),
+    [Start, Schrittgr, AnzSchritte, mittlen_ueber()]),
+  io:format("---------------------------------------------------------------------------------------~n"),
   if
     Typ == avl ->
-    %Aufruf mit zeitBT:messung(<Anzahl Startelemente>,<Schrittgroee>,<Anzahl Schritte>,<DurchlΣufe pro Messung>,<rand|auf|ab>
-    zeitAVLBT:messung(Start, Schrittgr, AnzSchritte, MittelnUeber, RandAufAb);
+      %Aufruf mit zeitBT:messung(<Anzahl Startelemente>,<Schrittgroee>,<Anzahl Schritte>,<DurchlΣufe pro Messung>,<rand|auf|ab>
+      zeitAVLBT:messung(Start, Schrittgr, AnzSchritte, mittlen_ueber(), RandAufAb);
+    Typ == splay ->
+      zeitsBT:messung(Start, Schrittgr, AnzSchritte, mittlen_ueber(), RandAufAb);
     true -> err
   end.
